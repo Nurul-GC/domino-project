1b8d6e2a5245e335f37aaf692c436fd404e51405
Nurul-GC <nuruldecarvalho@gmail.com>
2022-01-13 04:28:13 +0200

    atualizando tests/test_jogadores.py

--- tests/test_jogadores.py
+++ tests/test_jogadores.py
@@ -1,14 +1,18 @@
-from ia.jogadores import J7S
 from unittest import TestCase
+from ia.jogadores import J7S
 
 
 class TestJogadores(TestCase):
     def test_humano(self):
+        """testando o retorno da peca correspondente a jogada humana"""
         return self.assertTrue(
-                J7S().hm()
+                J7S()  # chamando a classe do nosso script responsavel pela organizacao dos jogadores
+                    .hm(5)  # chamando a funcao que retornara no terminal a peca corresdente a jogada
         )
 
     def test_computador(self):
+        """testando o retorno da peca correspondente a jogada computador"""
         return self.assertTrue(
-                J7S().pc()
+                J7S()  # chamando a classe do nosso script responsavel pela organizacao dos jogadores
+                    .pc(5)  # chamando a funcao que retornara no terminal a peca corresdente a jogada
         )

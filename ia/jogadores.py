885b15039d79e138ccd1c1b676bb13d679f0d0ec
Nurul-GC <nuruldecarvalho@gmail.com>
2022-01-13 02:25:40 +0200

    atualizando ia/jogadores.py

--- ia/jogadores.py
+++ ia/jogadores.py
@@ -10,6 +10,10 @@ class J7S:
         self.ppc = sample(self.pecas.pecas(), 7)
         self.hpc = sample(self.pecas.pecas(), 7)
 
+        # definindo em variavel a quantidade de pecas que cada jogador tem
+        self.qppc = len(self.ppc)
+        self.qhpc = len(self.hpc)
+
     def pc(self, valor_jogada: int):
         """calcula na lista de pecas atribuida ao jogador-pc
         se existe alguma peca com valor correspondente
@@ -19,10 +23,12 @@ class J7S:
         """
         for peca in self.ppc:
             if (peca[0] == valor_jogada) or (peca[1] == valor_jogada):
+                self.qppc -= 1
                 return peca
 
     def hm(self, numero_peca: int):
         """
         :return: a peca escolhida pelo jogador-humano;
         """
+        self.qhpc -= 1
         return self.hpc[numero_peca]

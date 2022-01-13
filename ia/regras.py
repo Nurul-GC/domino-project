87fd3355cbda992cef26d8f96d9cdd3071ac365f
Nurul-GC <nuruldecarvalho@gmail.com>
2022-01-13 04:18:43 +0200

    atualizando ia/regras.py

---
+++ ia/regras.py
@@ -0,0 +1,14 @@
+from ia.jogadores import J7S
+
+
+class R4S:
+    def __init__(self):
+        self.jogadores = J7S()
+        self.pecas_hm = self.jogadores.pecas_hm
+        self.pecas_pc = self.jogadores.pecas_pc
+
+    def bloqueio(self):
+        pass
+
+    def vitoria(self):
+        pass

c8618fb3d0144b4509129f5dc0339955f254d57e
Nurul-GC <nuruldecarvalho@gmail.com>
2022-01-13 02:26:09 +0200

    atualizando tests/test_pecas.py

---
+++ tests/test_pecas.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+from ia.pecas import P3S
+from unittest import TestCase
+
+
+class TestPecas(TestCase):
+    def test_domino(self):
+        """testando a solicitacao de uma peca"""
+        return self.assertTrue(
+                P3S()  # chamando a classe do nosso script responsavel pela organizacao das pecas
+                    .domino(numero_peca=5)  # chamando a funcao que imprimira no terminal a peca solicitada
+        )
+
+    def test_pecas(self):
+        """testando a impressao de todas as pecas do jogo"""
+        return self.assertTrue(
+                P3S()  # chamando a classe do nosso script responsavel pela organizacao das pecas
+                    .pecas()  # chamando a funcao que imprimira no terminal todas as pecas do jogo
+        )
